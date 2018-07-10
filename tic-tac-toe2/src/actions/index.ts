8a6f2f99ee163487842d49472282d7f7daa65979
Tomasz Kowal <tomasz.kowal@lingarogroup.com>
2018-07-10 17:02:55 +0200

    Before bugfix

---
+++ tic-tac-toe2/src/actions/index.ts
@@ -0,0 +1,18 @@
+// squareType- can be 'X' or 'O'
+export const checkSquare = (squareType, index) => ({
+    type: 'CHECK_SQUARE',
+    id: nextTodoId++,
+    squareType,
+    index
+});
+
+export const timeTravel = (moveIndex) => ({
+    type: 'TIME_TRAVEL',
+    moveIndex
+});
+
+export const fetchHistory = (index) => ({
+        type: 'FETCH_HISTORY',
+        index
+    }
+);
