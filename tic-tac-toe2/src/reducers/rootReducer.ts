16a3e37310eb35b8605aa736ef71d0a8d905f91e
Tomasz Kowal <tomasz.kowal@lingarogroup.com>
2018-07-17 14:08:59 +0200

    Bugfix: step index updated

--- tic-tac-toe2/src/reducers/rootReducer.ts
+++ tic-tac-toe2/src/reducers/rootReducer.ts
@@ -52,6 +52,7 @@ export default function rootReducer(state: TicTacToeState, action: any) //action
             state.history[state.stepIndex].squares[index] = 'O';
           }
 
+          state.stepIndex++;
           state.xIsNext = ((state.stepIndex%2) === 0);
 
           state.winner = calculateWinner(state.history[state.stepIndex].squares);
